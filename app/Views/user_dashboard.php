82c04d90c1b33d7610185507bf5a397ddedc440a
Aan Suseno <Aan Suseno>
2023-11-19 17:50:56 +0700

    feat: note in dashboard

--- app/Views/user_dashboard.php
+++ app/Views/user_dashboard.php
@@ -6,10 +6,14 @@
         <!-- Custom Tabs -->
         <div class="card">
             <div class="card-header d-flex p-0">
-            <h3 class="card-title p-3">Dashboard</h3>
+            <h3 class="card-title p-3">Note</h3>
             </div><!-- /.card-header -->
             <div class="card-body">
-                <h3>oke</h3>
+                <p>
+                    To avoid errors, please follow the steps carefully.
+                    If there is an issue in the previous step, you can delete the project and create it again.
+                    This application is still in development, and you can contribute on <a href="https://github.com/AanSuseno/spk-dss" target="_blank">GitHub</a> to improve it.
+                </p>
             <!-- /.tab-content -->
             </div><!-- /.card-body -->
         </div>
